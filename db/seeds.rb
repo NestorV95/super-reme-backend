e4bf857e48b27d3ce4087bc6c7896cc2acfaed39
RobertObi <robobi97@googlemail.com>
2021-05-12 17:29:01 -0500

    image data attached

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ OrderProduct.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# cat_array = ['', '']
-# cat_array[rand(0..(cat_array.length)]
+cat_array = ['Caps', 'Tops', 'Coats', 'Hoodies', 'Jackets', 'Socks', 'Scarves']
+product_array = ['https://assets.supremenewyork.com/211250/ma/C3nAHFB7B9U.jpg', 'https://assets.supremenewyork.com/204836/ma/GMmWvANQLWU.jpg', 'https://assets.supremenewyork.com/205030/ma/aETA_U6l6mY.jpg', 'https://assets.supremenewyork.com/205069/ma/mjYHiqVTLqw.jpg', 'https://assets.supremenewyork.com/204930/ma/VtiH1M_liCs.jpg', 'https://assets.supremenewyork.com/206145/ma/L0G5zYLdwIo.jpg', 'https://assets.supremenewyork.com/205983/ma/wBJaqXtTXR4.jpg']
 
 
 
-htx = Store.create(image: '', name: 'Super-reme HTX', address: "#{rand(1000..9999)}#{Faker::Address.street_name}", city: 'Houston', zip: Faker::Address.zip, phone: Faker::PhoneNumber.cell_phone, email: 'address@super-reme.com')
+htx = Store.create(image: 'https://d17ol771963kd3.cloudfront.net/assets/stores/store_11-4a142bdb15e1c9b13fca22701268ebc7.jpg', name: 'Super-reme HTX', address: "#{rand(1000..9999)}#{Faker::Address.street_name}", city: 'Houston', zip: Faker::Address.zip, phone: Faker::PhoneNumber.cell_phone, email: 'address@super-reme.com')
 
 10.times do 
-    Product.create(store: htx,name: Faker::Music.album, description: Faker::TvShows::AquaTeenHungerForce.quote,category: 'Shirt',image: '' , price: rand(1..1000), stock: rand(0..15))
+    Product.create(store: htx,name: Faker::Music.album, description: Faker::TvShows::AquaTeenHungerForce.quote,category: cat_array[rand(0..(cat_array.length))], image: product_array[rand(0..(product_array.length))] , price: rand(1..1000), stock: rand(0..15))
 end
 
 me = User.create(first_name:'Nestor', last_name: 'Venegas', username: 'yuh_boi',email:'address@email.com', password:'123',)
