ba7cfec9e9047f8a281e8e6255de561a74a6b4bb
Nestor <nestor.venegas95@gmail.com>
2021-05-13 11:35:23 -0500

    products fetch

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :authorized, only: [:index, :show]
   before_action :set_product, only: [:show]
 
   # GET /products
   def index
-    products = Product.all
-
-    render json: products
+    # products = Product.all
+    # token = encode_token(products: products )
+    # render json: {products: ProductSerializer.new(products)}, status: :accepted
+    render json: Product.all
   end
 
-  # # GET /products/1
-  def show
-    render json: product
-  end
+  # # # GET /products/1
+  # def show
+  #   render json: {product: ProductSerializer.new(product)}, status: :accepted
+  # end
 
   private
     # # Use callbacks to share common setup or constraints between actions.
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,5 +2,6 @@ class ProductSerializer < ActiveModel::Serializer
   belongs_to :store
   has_many :order_products
   has_many :orders, through: :order_products
-  attributes :id
+  attributes :id, :name, :description, :category, :image, :price, :stock 
+
 end
