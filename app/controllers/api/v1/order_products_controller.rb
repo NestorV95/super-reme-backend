d923a790db45c4aca4cf03bed997fa0775856406
Nestor <nestor.venegas95@gmail.com>
2021-05-12 19:13:32 -0500

    minor tweaks

--- app/controllers/api/v1/order_products_controller.rb
+++ app/controllers/api/v1/order_products_controller.rb
@@ -1,47 +1,38 @@
 class Api::V1::OrderProductsController < ApplicationController
-  before_action :set_order_product, only: [:show, :update, :destroy]
+  before_action :set_order_product, only: [:show, :destroy]
 
   # GET /order_products
   def index
-    @order_products = OrderProduct.all
+    order_products = OrderProduct.all
 
-    render json: @order_products
+    render json: order_products
   end
 
   # GET /order_products/1
   def show
-    render json: @order_product
+    render json: order_product
   end
 
   # POST /order_products
   def create
-    @order_product = OrderProduct.new(order_product_params)
+    order_product = OrderProduct.new(order_product_params)
 
-    if @order_product.save
-      render json: @order_product, status: :created, location: @order_product
+    if order_product.save
+      render json: order_product, status: :created, location: order_product
     else
-      render json: @order_product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /order_products/1
-  def update
-    if @order_product.update(order_product_params)
-      render json: @order_product
-    else
-      render json: @order_product.errors, status: :unprocessable_entity
+      render json: order_product.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /order_products/1
   def destroy
-    @order_product.destroy
+    order_product.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order_product
-      @order_product = OrderProduct.find(params[:id])
+      order_product = OrderProduct.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
